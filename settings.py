7d461b8f082bb241b3ecfbeabac765fbba6fbbd0
cherednichenko denis <leondc13@gmail.com>
2025-11-03 21:42:36 +0300

    minor fix

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-API_KEY = "a7a4d107-7e9d-4c50-82c8-1b65427e83be"
+API_KEY = ""
 BASE_URL_ROUTE = "https://routing.api.2gis.com/routing/7.0.0/global"
 BASE_URL_SEARCH = "https://catalog.api.2gis.com/3.0/items"
